f86656c15b4e1c86805f1d18f93fe81ecf6bf361
hassan <trondl-github@proton.me>
2026-08-10 20:00:44 +0200

    first commit

---
+++ src/main/java/com/project/ankaufformular/AnkaufformularApplication.java
@@ -0,0 +1,13 @@
+package com.project.ankaufformular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnkaufformularApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnkaufformularApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/ankaufformular/AnkaufformularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ankaufformular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnkaufformularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
